97df6a13f052d65fa4449ae2227633eae35b3d51
Adrian Dziergas <adriandziergas404@gmail.com>
2022-05-26 22:38:12 +0200

    added animation to nav

--- js/main.js
+++ js/main.js
@@ -1,17 +1,31 @@
 const nav = document.querySelector(".nav");
 const navBtn = document.querySelector(".burger-btn");
-const allNavItems = document.querySelectorAll('.nav__items');
+const allNavItems = document.querySelectorAll(".nav__item");
 
 // funkcja strzałkowa do wywołania nawigacji
 const handleNav = () => {
 	nav.classList.toggle("nav--active");
 
-    allNavItems.forEach(item => {
-        item.addEventListener('click', () => {
-            nav.classList.remove('nav--active')
-        })
-    })
+	allNavItems.forEach((item) => {
+		item.addEventListener("click", () => {
+			nav.classList.remove("nav--active");
+		});
+	});
+	// 4
+	handleNavItemsAnimation();
 };
+
+// 3
+const handleNavItemsAnimation = () => {
+	let delayTime = 0;
+
+	allNavItems.forEach((item) => {
+		item.classList.toggle("nav-items-animation");
+
+		// animation-delay: .3s
+		item.style.animationDelay = "." + delayTime + "s";
+		delayTime++;
+	});
+};
+
 navBtn.addEventListener("click", handleNav);
-
-
